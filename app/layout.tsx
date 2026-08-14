614f4fbc87728ee4ee3965107a8a7cbfca9de5ca
elvirafimansyah <freddyfirmansyah25@gmail.com>
2026-08-14 22:28:28 +0700

    fix(2): browser

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import './globals.css'
 import Navbar from "./navbar/page";
 import Footer from '@/components/footer';
 import { ThemeProvider } from "@/components/theme-provider"
